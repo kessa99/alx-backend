ae23b472929f09fd44a4e6912aec627f3cc2bcc6
root <kessadavidkipre@gmail.com>
2024-01-10 07:05:48 -0800

    add get_locale

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+create get_locale function with babel.selector
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    config class for app
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(config)
+
+
+@babel.localselectore
+def get_locale():
+    """
+    determine the best match
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home():
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
